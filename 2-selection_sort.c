f95687ef038f2fdf4588c1aa031d5515725e69ef
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-11-21 17:31:34 +0100

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers using the selection sort algorithm.
+ * selection_sort - Sorts an array of integers in ascending order using the
+ *                  Selection sort algorithm.
  * @array: The array to be sorted.
  * @size: Number of elements in the array.
  */
